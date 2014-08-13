44dbf9833fa5b084864bb8898351976cc4dd8811
SalTorc <salvatore.torcivia93@gmail.com>
2014-08-12 21:39:54 -0400

    Got list to work, and it updates the project title so far

--- customAssets/js/projects.js
+++ customAssets/js/projects.js
@@ -1,25 +1,23 @@
 $(document).ready(function(){
-	var $projectTitle = $('#projectTitle');
-	var $imgUrl = $('#imgUrl');
-	var $descriptionTitle = $('#descriptionTitle');
-	var $description = $('#description');
-	var $urlText = $('#urlText');
-	var $projectList = $('#projectList')
+	var $projectList = $('#projectList');
+
 	$.getJSON('../projectsInfo.json', function(result){
+		console.log("== [ $.getJSON - Success ] ==");
 		$.each(result.projects, function(key, value){
-			$projectList.append(''
-				+'<li class="col-md-4 text-center" data-os-animation="" data-os-animation-delay="">'
-				+	'<div class="box-round box-medium">'
-				+		'<div class="box-dummy"></div>'
-				+		'<a class="box-inner" href="#">'
-				+			'<img src="' + value.imgUrl + '">'
-				+		'</a>'
-				+	'</div>'
-				+	'<h3 class="text-center ">'
-				+		'<a href="#">' + value.projectTitle + '</a>'
-				+	'</h3>'
-				+'</li>'
-			);
+			$projectList.append('<li class="col-md-2 text-center" data-os-animation="" data-os-animation-delay="">'+
+				'<div class="box-round box-medium">'+
+					'<div class="box-dummy"></div>'+
+					'<a class="box-inner" href="javascript:void(0)">'+
+						'<img alt="' + value.identifier + '" src="' + value.imgUrl + '" onclick="moreInfo(this)">'+
+					'</a>'+
+				'</div>'+
+				'<h3 class="text-center">'+
+					'<a href="javascript:void(0)">' + value.projectTitle + '</a>'+
+				'</h3>'+
+			'</li>');
 		});
+	}).fail(function(jqxhr, textStatus, error){
+		var err = textStatus + ', ' + error;
+		console.log("== [ Request Failed: " + err + " ] ==");
 	});
 });
