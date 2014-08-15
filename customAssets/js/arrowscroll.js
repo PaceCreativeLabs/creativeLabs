3258bc8dcbe2295a43e74eb99f8798e528e6ecd0
Barak Michaely <bm09148n@pace.edu>
2014-08-15 16:39:24 -0400

    so many changes...

---
+++ customAssets/js/arrowscroll.js
@@ -0,0 +1,5 @@
+$('.arrow-down').click(function(event) {
+    event.preventDefault();
+    var pos = $($(this).attr('href')).position().top;
+    $('html, body').animate({scrollTop: pos}, 300);
+});
