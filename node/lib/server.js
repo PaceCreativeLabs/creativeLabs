7c36ee0b5c5a533edaf4ad5c9c3fa13d5ff76693
Barak Michaely <bm09148n@pace.edu>
2014-08-25 12:23:32 -0400

    umm node server and layout stuff

--- node/lib/server.js
+++ node/lib/server.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-	scrape = require('./scrape'),
+	//scrape = require('./scrape'),
 	path = require('path'),
 	app = express();
 
@@ -7,9 +7,20 @@ app.use(express.static('../../html'));
 
 app.engine('html', require('ejs').renderFile);
 
+// simple logger
+// app.use(function(req, res, next){
+//   console.log('%s %s', req.method, req.url);
+//   next();
+// });
+
+// // respond
+// app.use(function(req, res, next){
+//   res.send('Hello World');
+// });
+
 app.get('/home', function (req,res) {
-	res.sendfile('html/index.html',{'root': '../'});
+	res.sendfile('index.html',{'root': '../html'});
 });
 
 app.listen(5000);
-console.log('Listening SCL');
+console.log('Listening SCL node');
