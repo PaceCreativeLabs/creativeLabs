2a005cc92a410e127dae05d9f29ae6cbbbf2dd7a
SalTorc <salvatore.torcivia93@gmail.com>
2014-09-12 18:34:20 -0400

    what

--- customAssets/js/removeFrame.js
+++ customAssets/js/removeFrame.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
 	var content = document.getElementById('content');
     var span = content.getElementsByTagName('frame')[0];
     var p = content.getElementsByTagName(' ')[0];
+    content.innerHTML = span.innerHTML;
 });
