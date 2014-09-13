f0cd8b140d2ef792f6624b55ba651c1a2139f18d
SalTorc <salvatore.torcivia93@gmail.com>
2014-09-13 17:43:20 -0400

    Removed framescript

--- customAssets/js/removeFrame.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	var content = document.getElementById('content');
-    var span = content.getElementsByTagName('frame')[0];
-    var p = content.getElementsByTagName(' ')[0];
-    content.innerHTML = span.innerHTML;
-});
