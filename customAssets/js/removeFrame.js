6f6889c5f28d03aa49f577cbc22b947906402ce5
Barak Michaely <barakm18@gmail.com>
2014-10-26 23:04:14 -0400

    smallish update

---
+++ customAssets/js/removeFrame.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	var content = document.getElementById('content');
+    var span = content.getElementsByTagName('frame')[0];
+    var p = content.getElementsByTagName(' ')[0];
+});
