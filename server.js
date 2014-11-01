781cd964eacd67bce5f3ce3e407ff38e00650e05
SalTor <salvatore.torcivia93@gmail.com>
2014-11-01 00:15:12 -0400

    Change back to using res.sendFile due to res.status().send() not working properly, also minimized size of file

--- server.js
+++ server.js
@@ -1,42 +1,13 @@
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 80));
+var express = require('express'), app = express();
+app.set('port',(process.env.PORT || 5000));
 app.use(express.static(__dirname + '/html'));
 app.use(express.static(__dirname));
-
-app.get('/home', function (req,res) {
-	res.redirect('/');
-});
-
-app.get('/team', function (req,res) {
-	res.status(200).send('aboutandteam.html',{'root':'./html'});
-});
-
-app.get('/skills', function (req,res) {
-	res.status(200).send('services.html');
-});
-
-app.get('/projects', function (req,res) {
-	res.status(200).send('projects.html');
-});
-
-app.get('/join', function (req,res) {
-	res.status(200).send('join.html');
-});
-
-app.get('/contact', function (req,res) {
-	res.status(200).send('contact.html');
-});
-
-app.get('/project', function (req,res) {
-	res.status(200).send('singleproject.html');
-});
-
-app.get('*', function(req, res){
-  res.redirect('/');
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
+app.get('*',function(req, res){res.redirect('/');});
+app.get('/home',function(req,res){res.redirect('/');});
+app.get('/join',function(req,res){res.sendfile('join.html',{'root':'./html'});});
+app.get('/skills',function(req,res){res.sendfile('services.html',{'root':'./html'});});
+app.get('/contact',function(req,res){res.sendfile('contact.html',{'root':'./html'});});
+app.get('/projects',function(req,res){res.sendfile('projects.html',{'root':'./html'});});
+app.get('/team',function(req,res){res.sendfile('aboutandteam.html',{'root':'./html'});});
+app.get('/project',function(req,res){res.sendfile('singleproject.html',{'root':'./html'});});
+app.listen(app.get('port'),function(){console.log("Node app is running at localhost:" + app.get('port'));});
