8f152e3846da2449b119c748ebbe821a590fa0ca
SalTor <salvatore.torcivia93@gmail.com>
2014-11-01 16:48:48 -0400

    Server on port 80

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),app = express();
 errorHandler = function(err,req,res,next){res.redirect('/');};
-app.set('port',(process.env.PORT || 5000));
+app.set('port',(process.env.PORT || 80));
 app.use(express.static(__dirname + '/html'));
 app.use(express.static(__dirname));
 app.get('/:page',function(req,res){res.sendFile(req.params.page + '.html',{'root':'./html'});}).use(errorHandler);
